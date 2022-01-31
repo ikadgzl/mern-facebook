60cf26a6635736f4924c44cba9f4e2b4bd8c5a6b
ikadgzl <ikadgzl@gmail.com>
2022-01-31 17:54:37 +0300

    refactoring

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,8 +3,6 @@ import { loginUser, registerUser } from '../controller/auth.js';
 
 const authRouter = Router();
 
-authRouter.get('/', (req, res) => res.send('from auth router'));
-
 // Register Route
 authRouter.post('/register', registerUser);
 
